01d9895bd46a01e35064396f4a6ab4dee296ae81
MKBarhoumi <mhmdkamalalsadq@gmail.com>
2026-02-24 12:57:37 +0100

    Refactor invoice total calculation to include discount and improve tax summary structure

--- app/Services/InvoiceCalculationService.php
+++ app/Services/InvoiceCalculationService.php
@@ -16,13 +16,14 @@ class InvoiceCalculationService
      *   total_ht: string,
      *   total_net_before_disc: string,
      *   total_gross: string,
+     *   total_discount: string,
      *   total_tva: string,
      *   timbre_fiscal: string,
      *   total_ttc: string,
      *   total_allowances: string,
      *   total_charges: string,
-     *   tax_summary: array<string, array{taxable_amount: string, tax_amount: string, rate: string}>,
-     *   lines: array<int, array{line_net_amount: string, tva_amount: string, discount_amount: string}>
+     *   tax_summary: array<int, array{tax_type_code: string, tax_type_name: string, tax_rate: string, taxable_amount: string, tax_amount: string}>,
+     *   lines: array<int, array{line_number: int, line_net_amount: string, tva_amount: string, discount_amount: string, line_total: string}>
      * }
      */
     public function calculateTotals(Invoice $invoice, string $timbreFiscalAmount = '0.000', bool $timbreEnabled = false): array
@@ -30,26 +31,34 @@ class InvoiceCalculationService
         $totalHt = '0.000';
         $totalNetBeforeDisc = '0.000';
         $totalTva = '0.000';
-        $totalAllowances = '0.000';
+        $totalDiscount = '0.000';
         $totalCharges = '0.000';
         $taxSummary = [];
         $lineResults = [];
 
-        foreach ($invoice->lines as $index => $line) {
+        foreach ($invoice->lines as $line) {
             $lineCalc = $this->calculateLine($line);
-            $lineResults[$index] = $lineCalc;
+            $lineResults[] = [
+                'line_number' => $line->line_number,
+                'line_net_amount' => $lineCalc['line_net_amount'],
+                'tva_amount' => $lineCalc['tva_amount'],
+                'discount_amount' => $lineCalc['discount_amount'],
+                'line_total' => bcadd($lineCalc['line_net_amount'], $lineCalc['tva_amount'], 3),
+            ];
 
             $totalNetBeforeDisc = bcadd($totalNetBeforeDisc, bcmul($line->quantity, $line->unit_price, 3), 3);
             $totalHt = bcadd($totalHt, $lineCalc['line_net_amount'], 3);
             $totalTva = bcadd($totalTva, $lineCalc['tva_amount'], 3);
-            $totalAllowances = bcadd($totalAllowances, $lineCalc['discount_amount'], 3);
+            $totalDiscount = bcadd($totalDiscount, $lineCalc['discount_amount'], 3);
 
             $rateKey = $line->tva_rate;
             if (!isset($taxSummary[$rateKey])) {
                 $taxSummary[$rateKey] = [
+                    'tax_type_code' => 'VAT',
+                    'tax_type_name' => 'TVA',
+                    'tax_rate' => $rateKey,
                     'taxable_amount' => '0.000',
                     'tax_amount' => '0.000',
-                    'rate' => $rateKey,
                 ];
             }
             $taxSummary[$rateKey]['taxable_amount'] = bcadd($taxSummary[$rateKey]['taxable_amount'], $lineCalc['line_net_amount'], 3);
@@ -57,19 +66,20 @@ class InvoiceCalculationService
         }
 
         $timbreFiscal = $timbreEnabled ? $timbreFiscalAmount : '0.000';
-        $totalGross = bcadd($totalHt, $totalAllowances, 3);
+        $totalGross = bcadd($totalHt, $totalDiscount, 3);
         $totalTtc = bcadd(bcadd($totalHt, $totalTva, 3), $timbreFiscal, 3);
 
         return [
             'total_ht' => $totalHt,
             'total_net_before_disc' => $totalNetBeforeDisc,
             'total_gross' => $totalGross,
+            'total_discount' => $totalDiscount,
             'total_tva' => $totalTva,
             'timbre_fiscal' => $timbreFiscal,
             'total_ttc' => $totalTtc,
-            'total_allowances' => $totalAllowances,
+            'total_allowances' => $totalDiscount,
             'total_charges' => $totalCharges,
-            'tax_summary' => $taxSummary,
+            'tax_summary' => array_values($taxSummary),
             'lines' => $lineResults,
         ];
     }
